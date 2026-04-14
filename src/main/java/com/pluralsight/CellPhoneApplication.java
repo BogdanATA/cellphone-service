8c9685159e24e10066ca73ff7dc5ddac73d09282
BogdanATA <batameev@gmail.com>
2026-04-14 15:00:26 -0400

    created the get user info and set those values section

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,7 +1,31 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class CellPhoneApplication {
     public static void main(String[] args) {
+
+        CellPhone phone = new CellPhone();
+
+        Scanner scanner = new Scanner(System.in);
+
+        //get user info and set values
+        System.out.println("What is the serial number? ");
+        phone.setSerialNumber(Integer.parseInt(scanner.nextLine()));
+
+        System.out.println("What model is the phone? ");
+        phone.setPhoneNumber(scanner.nextLine());
+
+        System.out.println("Who is the carrier? ");
+        phone.setModel(scanner.nextLine());
+
+        System.out.println("What is the phone number? ");
+        phone.setPhoneNumber(scanner.nextLine());
+
+        System.out.println("Who is the owner of the phone");
+        phone.setOwner(scanner.nextLine());
+
+
 
     }
 }
